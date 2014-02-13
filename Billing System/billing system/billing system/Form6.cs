824d47abe13f130c1f0f757fa3c82a50155ab26c
Ravisha Heshan <ravishaheshan@gmail.com>
2014-02-13 17:26:40 +0530

    Revert "Form6 deleted"
    This reverts commit 6afd45dacb4cd2a12a69cdb1c522bcf75d1d6e80.

---
+++ Billing System/billing system/billing system/Form6.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace billing_system
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+    }
+}
